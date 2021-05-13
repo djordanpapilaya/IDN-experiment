b1814077679fb422a8825a7103c424e9ab63a553
djordanpapilaya <djordan@mrdjordan.amsterdam>
2021-05-13 10:11:42 +0100

    Fix user token not being saved

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -65,7 +65,7 @@ class RegisteredUserController extends Controller
 			'last_name' => $request->last_name,
 			'email' => $request->email,
 			'password' => Hash::make($request->password),
-			'user_token' => $this->userToken,
+			'user_token' => $request->user_token,
 		]);
 
 		event(new Registered($user));
