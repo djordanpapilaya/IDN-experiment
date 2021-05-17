c09b5bb68ef17fac1212ad332c5c5215923bb60c
djordanpapilaya <djordan@mrdjordan.amsterdam>
2021-05-17 13:29:33 +0100

    Fix auth to set session when authenticated and directly login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sessions;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -42,6 +43,23 @@ class RegisteredUserController extends Controller
 		return view('auth.register')->with(array('usertoken'=>$this->userToken,'firstName'=>$this->firstname,'lastName'=>$this->lastname));
 	}
 
+	public function createSession(Request $request)
+	{
+		$sessionObj = new Sessions;
+
+		$user_id = \Auth::user()->id;
+		$ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
+		$agent = $request->server('HTTP_USER_AGENT');
+		$current_time = \Carbon\Carbon::now()->timestamp;
+
+		$sessionObj::create([
+			'user_id' => $user_id,
+			'ip_address' => $ip,
+			'user_agent' => $agent,
+			'start_time' => $current_time,
+		]);
+	}
+
 	/**
 	 * Handle an incoming registration request.
 	 *
@@ -71,6 +89,8 @@ class RegisteredUserController extends Controller
 		event(new Registered($user));
 
 		Auth::login($user);
+
+		$this->createSession($request);
 
 		return redirect(RouteServiceProvider::HOME);
 	}
