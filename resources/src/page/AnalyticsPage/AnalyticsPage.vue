93d6dda8d76f9ae466a117bcc7fc094e494a3fd2
djordanpapilaya <djordan@mrdjordan.amsterdam>
2021-06-24 14:08:02 +0200

    Adding more stats to analytics page

--- resources/src/page/AnalyticsPage/AnalyticsPage.vue
+++ resources/src/page/AnalyticsPage/AnalyticsPage.vue
@@ -11,14 +11,36 @@
     </div>
 
     <div :class="[$style.graphs]" v-if="chartAvailable">
-      <div>
+      <div :class="$style.graphWrapper">
         <apexchart width="450" type="line" :options="options" :series="series" ref="polical"></apexchart>
+        <br><br>
+        <p :class="'copy-04'">RELATIVE VALUES</p>
+        <p :class="'copy-04'">Conservative: {{(user.all.stats.political_conservative / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Middle: {{(user.all.stats.political_center / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Progressive: {{(user.all.stats.political_progressive / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Start point: {{user.all.watched[0].political}}</p>
+        <p :class="'copy-04'">End point: {{user.all.watched[user.all.stats.numb_of_sources -1].political}}</p>
+
       </div>
-      <div>
+      <div :class="$style.graphWrapper">
         <apexchart width="450" type="line" :options="optionsType" :series="seriesType" ref="type"></apexchart>
+        <br><br>
+        <p :class="'copy-04'">RELATIVE VALUES</p>
+        <p :class="'copy-04'">Video: {{(user.all.stats.type_video / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Audio: {{(user.all.stats.type_audio / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Text: {{(user.all.stats.type_text / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Start point: {{user.all.watched[0].type}}</p>
+        <p :class="'copy-04'">End point: {{user.all.watched[user.all.stats.numb_of_sources -1].type}}</p>
       </div>
-      <div>
+      <div :class="$style.graphWrapper">
         <apexchart width="450" type="line" :options="optionsComplexity" :series="seriesComplexity" ref="complexity"></apexchart>
+        <br><br>
+        <p :class="'copy-04'">RELATIVE VALUES</p>
+        <p :class="'copy-04'">Simple: {{(user.all.stats.content_simple / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Medium: {{(user.all.stats.content_medium / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Complex: {{(user.all.stats.content_complex / user.all.stats.numb_of_sources).toFixed(3)}}</p>
+        <p :class="'copy-04'">Start point: {{user.all.watched[0].content}}</p>
+        <p :class="'copy-04'">End point: {{user.all.watched[user.all.stats.numb_of_sources -1].content}}</p>
       </div>
     </div>
   </div>
